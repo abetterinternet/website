7993dc1b8b63c418c1bcc5e0cb4f960270356387
Josh Aas <jaas@kflag.net>
2019-11-25 23:40:43 -0500

    Update css build script and custom theme variables.

--- css-build.sh
+++ css-build.sh
@@ -4,7 +4,7 @@
 # npm install -g node-sass gulp gulp-cli
 
 # Store the theme package filename
-theme_filename='purpose-website-ui-kit-v1.1.3'
+theme_filename='purpose-website-ui-kit-v2.0.1'
 
 # Save current directory
 site_dir=$(pwd)
@@ -17,10 +17,10 @@ rm -Rf /tmp/__MACOSX
 unzip ~/Downloads/${theme_filename}.zip -d /tmp/
 
 # Copy custom CSS to build directory
-rm /tmp/${theme_filename}/assets/scss/user/_styles.scss
-rm /tmp/${theme_filename}/assets/scss/user/_variables.scss
-cp ./src/purpose-user-css/_styles.scss    /tmp/${theme_filename}/assets/scss/user/
-cp ./src/purpose-user-css/_variables.scss /tmp/${theme_filename}/assets/scss/user/
+rm /tmp/${theme_filename}/resources/scss/custom/_styles.scss
+rm /tmp/${theme_filename}/resources/scss/custom/_variables.scss
+cp ./src/purpose-user-css/_styles.scss    /tmp/${theme_filename}/resources/scss/custom/
+cp ./src/purpose-user-css/_variables.scss /tmp/${theme_filename}/resources/scss/custom/
 
 # Build theme files
 cd /tmp/${theme_filename}/
@@ -32,8 +32,8 @@ rm ${site_dir}/static/purpose-theme/css/theme.css
 rm ${site_dir}/static/purpose-theme/css/theme.min.css
 
 # Copy over build results
-cp ./dist/css/theme.css     ${site_dir}/static/purpose-theme/css/
-cp ./dist/css/theme.min.css ${site_dir}/static/purpose-theme/css/
+cp ./assets/css/purpose.css     ${site_dir}/static/purpose-theme/css/theme.css
+cp ./assets/css/purpose.min.css ${site_dir}/static/purpose-theme/css/theme.min.css
 
 # Clean up
 rm -Rf /tmp/${theme_filename}
