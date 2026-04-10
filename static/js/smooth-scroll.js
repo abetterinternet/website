a8b33a2b8308b3a8fd75aac97f7dba8fc48781b4
scottmakestech <83726258+scottmakestech@users.noreply.github.com>
2026-04-10 10:38:41 -0500

    Update promo message, link to footer

---
+++ static/js/smooth-scroll.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.js-smooth-scroll').forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    var target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      e.preventDefault();
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
